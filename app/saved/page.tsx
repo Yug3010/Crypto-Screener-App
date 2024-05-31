9137837c6a7b14dd0002efd9b6fa47b567c2191a
Yug3010 <yugsutariya4i@gmail.com>
2024-05-31 02:22:20 -0400

    updates done by yug

---
+++ app/saved/page.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Home from '../page'
+import Logo from '../components/Logo'
+import Navigation from '../components/Navigation'
+
+const page = () => {
+  return (
+    <>
+    <main
+        className="w-full h-full flex flex-col first-letter:
+    content-center items-center relative text-white font-nunito
+    "
+      >
+
+        <div className="w-screen h-screen bg-gray-300 fixed -z-10" />
+        <Logo/>
+        <Navigation/>
+        Saved
+        </main>
+    </>
+  )
+}
+
+export default page
